d3a94baefd296ff379bd9416b67afce81e4a246c
Miguel Rivas <mrivaslezama@gmail.com>
2023-04-24 12:54:43 -0400

    getting css right

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+@import "bootstrap-sprockets";
+@import "bootstrap";
+@import "font-awesome";
+@import "jquery.datetimepicker";
+@import "*";
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
+//= require_tree .
